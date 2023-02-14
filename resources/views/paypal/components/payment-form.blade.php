f4b3d23a5eac160e45f87e1d184f557042c9d7ae
Kyle Anderson <kyle@creative2.com>
2023-02-14 16:27:11 -0500

    styling payment button

--- resources/views/paypal/components/payment-form.blade.php
+++ resources/views/paypal/components/payment-form.blade.php
@@ -59,8 +59,9 @@
             </div>
             <button
                 type="submit"
-                class="flex items-center my-4 px-5 py-3 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500 disabled:opacity-50">
-                Submit
+                class="flex items-center space-x-2 font-bold my-4 px-5 py-3 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500 disabled:opacity-50">
+                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M6 9V7.25C6 3.845 8.503 1 12 1s6 2.845 6 6.25V9h.5a2.5 2.5 0 012.5 2.5v8a2.5 2.5 0 01-2.5 2.5h-13A2.5 2.5 0 013 19.5v-8A2.5 2.5 0 015.5 9H6zm1.5-1.75C7.5 4.58 9.422 2.5 12 2.5c2.578 0 4.5 2.08 4.5 4.75V9h-9V7.25zm-3 4.25a1 1 0 011-1h13a1 1 0 011 1v8a1 1 0 01-1 1h-13a1 1 0 01-1-1v-8z"></path></svg>
+                <span>Pay {{ $cart->total->formatted() }}</span>
             </button>
         </form>
     </div>
