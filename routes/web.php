0f3df4a3da31454780abe2d0bfa92ad115f2348b
kylekanderson <kylekanderson@users.noreply.github.com>
2023-02-21 21:10:55 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Lancodev\LunarPaypal\Http\Controllers\OrdersController;
-use Lancodev\LunarPaypal\PaypalPaymentType;
-use Lunar\Models\Cart;
-use Lunar\Models\Transaction;
-use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
 Route::prefix('lunar-paypal')->group(function () {
     Route::post('/orders', [OrdersController::class, 'create'])->name('lunar-paypal.orders.create');
